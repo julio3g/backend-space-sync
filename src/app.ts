6892273854bb6bf2e53d2978eddf04dbc4947ca3
Julio Cesar Orso <juliocesar.orso@gmail.com>
2024-03-19 21:56:16 -0300

    error validation on route messages

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import cors from '@fastify/cors'
 import multipart from '@fastify/multipart'
+import { randomUUID } from 'crypto'
 import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import { env } from 'process'
 import { ZodError, z } from 'zod'
 import { AppError } from './appError'
+import { r2 } from './lib/cloudflare'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 app.register(multipart)
@@ -12,24 +17,14 @@ app.register(multipart)
 
 app.post('/messages', async (request: FastifyRequest, reply: FastifyReply) => {
   const createMessageBodySchema = z.object({
-    name: z.string().optional(),
-    dateTime: z.date().optional(),
-    delayTime: z.number().optional(),
-    status: z.string().optional(),
-    copywriting: z.string().optional(),
-    // files: z.array(
-    //   z.object({
-    //     name: z.string(),
-    //     key: z.string(),
-    //     contentType: z.string(),
-    //   }),
-    // ),
+    name: z.string(),
+    dateTime: z.coerce.date(),
+    delayTime: z.number(),
+    copywriting: z.string(),
   })
 
   const { name, dateTime, delayTime, copywriting } =
     createMessageBodySchema.parse(request.body)
-
-  const getFiles = await request.saveRequestFiles()
 
   // if (getFiles.length === 1) {
   //   console.log(getFiles[0].filename)
@@ -41,20 +36,81 @@ app.post('/messages', async (request: FastifyRequest, reply: FastifyReply) => {
   //     })),
   //   )
   // }
+  const messageIdGenerator = randomUUID()
+  const files = await request.saveRequestFiles()
 
-  const data = {
-    name,
-    dateTime: new Date(dateTime ?? new Date()),
-    delayTime,
-    copywriting,
-    // files: getFiles.map((file) => ({
-    //   name: file.filename,
-    //   key: randomUUID().concat('-').concat(file.filename),
-    //   contentType: file.mimetype,
-    // })),
-  }
-  // getFiles.map((file) => file.filename)
-  return reply.status(201).send(data)
+  const allFiles = await Promise.all(
+    files.map(async ({ filename, mimetype }) => {
+      const fileKey = `${randomUUID()}-${filename}`
+      const signedUrl = await getSignedUrl(
+        r2,
+        new PutObjectCommand({
+          Bucket: 'space-sync-dev',
+          Key: fileKey,
+          ContentType: mimetype,
+        }),
+        { expiresIn: 600 },
+      )
+
+      return { name: filename, key: fileKey, signedUrl, contentType: mimetype }
+    }),
+  )
+
+  const filesInMessage = allFiles.map(
+    async ({ name, key, contentType, signedUrl }) => {
+      await prisma.file.createMany({
+        data: [
+          {
+            name,
+            contentType,
+            key,
+            messageId: messageIdGenerator,
+          },
+        ],
+      })
+      return { signedUrl, key }
+    },
+  )
+
+  const message = await prisma.message.create({
+    data: {
+      id: messageIdGenerator,
+      name,
+      dateTime,
+      delayTime,
+      copywriting,
+    },
+  })
+
+  return reply.status(201).send({ message, filesInMessage })
+})
+
+app.post('/upload', async (request: FastifyRequest, reply: FastifyReply) => {
+  // const uploadBodySchema = z.object({
+  //   name: z.string().min(1),
+  //   contentType: z.string().regex(/\w+\/[-+.\w]+/g),
+  // })
+  // const { name, contentType } = uploadBodySchema.parse(request.body)
+  const files = await request.saveRequestFiles()
+
+  const allFiles = await Promise.all(
+    files.map(async ({ filename, mimetype }) => {
+      const fileKey = `${randomUUID()}-${filename}`
+      const signedUrl = await getSignedUrl(
+        r2,
+        new PutObjectCommand({
+          Bucket: 'space-sync-dev',
+          Key: fileKey,
+          ContentType: mimetype,
+        }),
+        { expiresIn: 600 },
+      )
+
+      return { fileKey, signedUrl }
+    }),
+  )
+
+  return reply.status(201).send(allFiles)
 })
 
 app.register(cors, {
